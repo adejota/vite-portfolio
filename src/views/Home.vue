e0e9dc76e6909f5bcf9034a6552d6cce427be0a6
Ademir Diamente Júnior <ademir.diamentejunior@gmail.com>
2022-09-26 09:02:42 -0300

    feature resume

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -68,11 +68,11 @@
       class="w-full p-4 mb-14 bg-mid-black rounded-xl border border-extra-low-white overflow-hidden animate__animated animate__fadeIn"
     >
       <h2 class="text-2xl text-full-white font-bold">About Me</h2>
-      <div class="mt-2 mb-4 w-12 border-2 rounded-xl border-primary bg-primary"></div>
+      <div class="mt-2 mb-6 w-12 border-2 rounded-xl border-primary bg-primary"></div>
       <p class="text-mid-white mb-2">Sou graduado em contabilidade desde 2018 e trabalho na área de tecnologia desde 2020.</p>
       <p class="text-mid-white mb-2">Realizei um curso técnico de multimídia integrado ao ensino médio onde aprimorei o meu lado criativo e adquiri habilidades de UX e UI.</p>
       <p class="text-mid-white mb-2">Durante a faculdade e meus primeiros empregos trabalhei bastante o meu lado de negócios, conhecimento que considero crucial para entender mais a fundo a solução que as empresas oferecem.</p>
-      <p class="text-mid-white">Atualmente estou atuando como desenvolvedor frontend na Znap Technologies, estudando testes unitários, e2e e aprimorando meus conhecimentos no framework Vue.</p>
+      <p class="text-mid-white mb-6">Atualmente estou atuando como desenvolvedor frontend na Znap Technologies, estudando testes unitários, e2e e aprimorando meus conhecimentos no framework Vue.</p>
 
       <div class="grid grid-cols-2 gap-4 mt-4 mb-8">
         <div v-for="aboutListItem in aboutListItems" :key="aboutListItem.title"
@@ -107,14 +107,39 @@
       class="w-full p-4 mb-14 bg-mid-black rounded-xl border border-extra-low-white overflow-hidden animate__animated animate__fadeIn"
     >
       <h2 class="text-2xl text-full-white font-bold">Resume</h2>
-      <div class="mt-2 mb-4 w-12 border-2 rounded-xl border-primary bg-primary"></div>
+
+      <div class="mt-2 mb-6 w-12 border-2 rounded-xl border-primary bg-primary"></div>
+
+      <h3 class="text-lg text-full-white font-bold mb-4">Education</h3>
+      <ul class="text-full-white marker:text-primary list-outside list-disc ml-6 mb-4">
+        <li v-for="educationListItem in educationListItems" :key="educationListItem.title"
+          class="pb-4"
+        >
+          <h4 class="font-bold">{{ educationListItem.title }}</h4>
+          <p class="text-primary font-bold">{{ educationListItem.subtitle }}</p>
+          <p class="text-mid-white">{{ educationListItem.content }}</p>
+        </li>
+      </ul>
+
+      <h3 class="text-lg text-full-white font-bold mb-4">Experience</h3>
+      <ul class="text-full-white marker:text-primary list-outside list-disc ml-6 mb-4">
+        <li v-for="experienceListItem in experienceListItems" :key="experienceListItem.title"
+          class="pb-4"
+        >
+          <h4 class="font-bold">{{ experienceListItem.title }}</h4>
+          <p class="text-primary font-bold">{{ experienceListItem.subtitle }}</p>
+          <p class="text-mid-white">{{ experienceListItem.content }}</p>
+        </li>
+      </ul>
+
+      <h3 class="text-lg text-full-white font-bold mb-4">My skills</h3>
     </section>
 
     <section v-if="selectedNavBarItem === 3"
       class="w-full p-4 mb-14 bg-mid-black rounded-xl border border-extra-low-white overflow-hidden animate__animated animate__fadeIn"
     >
       <h2 class="text-2xl text-full-white font-bold">Portfolio</h2>
-      <div class="mt-2 mb-4 w-12 border-2 rounded-xl border-primary bg-primary"></div>
+      <div class="mt-2 mb-6 w-12 border-2 rounded-xl border-primary bg-primary"></div>
 
       <div v-for="portfolioListItem in portfolioListItems" :key="portfolioListItem.name"
         class="animate__animated animate__zoomIn animate__faster"
@@ -187,6 +212,30 @@ export default {
       doingListItems: [
         { title: 'Web Development', content: 'High-quality development of sites at the professional level', icon: 'CodeBracketSquare' },
         { title: 'Web Design', content: 'The most modern and high-quality design made at a professional level', icon: 'PencilSquare' },
+      ],
+
+      educationListItems: [
+        { title: 'Universidade de São Paulo', subtitle: '2013 — 2018', content: 'Bachelors, Accounting'},
+        { title: 'Université Paris Dauphine', subtitle: '2016 — 2016', content: 'Exchange program, Business'},
+      ],
+
+      experienceListItems: [
+        { title: 'Frontend Developer', subtitle: 'Znap Technologies | mar 2021 — present', content: 'Atuo com foco no front-end em um sistema Vue.js modularizado em diferentes SPAs. Sou responsável por desenvolver componentes genéricos que seguem o princípio de low-coding e são utilizados em todas as views do sistema. Contribuo com code-review dos meus pares, além de ser o responsável por controlar o versionamento do código e o deploy do sistema' },
+        { title: 'Product Owner', subtitle: 'Lavapp | mar 2020 — mar 2021', content: 'Atuei como PO em uma webview com front-end em Vue.js e API Laravel. Também fui responsável por elaborar relatórios gerenciais com objetivo de criar e analisar indicadores estratégicos para a área comercial e financeira' },
+        { title: 'Analista', subtitle: 'Petroplus | feb 2018 — feb 2020', content: 'Atuei auxiliando decisões de nível gerencial com foco na melhoria de processos e planejamento estratégico' },
+        { title: 'Estagiário comercial', subtitle: 'Petroplus | mar 2017 — jan 2018', content: '' },
+        { title: 'Estagiário de contabilidade', subtitle: 'Banco Safra | oct 2015 — jul 2016', content: '' },
+      ],
+
+      skillsListItems: [
+        { title: 'HTML/CSS', percentage: 95 },
+        { title: 'Javascript', percentage: 90 },
+        { title: 'Vue.js', percentage: 90 },
+        { title: 'Tailwind', percentage: 90 },
+        { title: 'Vuetify', percentage: 90 },
+        { title: 'GitHub', percentage: 90 },
+        { title: 'MySQL', percentage: 90 },
+        { title: 'Node.js', percentage: 90 },
       ],
 
       portfolioListItems: [
